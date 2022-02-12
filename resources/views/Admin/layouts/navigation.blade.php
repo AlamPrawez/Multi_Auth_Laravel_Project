07cdf0c21c7d7797b599ea11afa116becad9f552
prawez <praweza05@gmail.com>
2022-02-12 18:10:01 +0545

    some miner update

--- resources/views/Admin/layouts/navigation.blade.php
+++ resources/views/Admin/layouts/navigation.blade.php
@@ -39,7 +39,7 @@
                         <form method="POST" action="{{ route('admin.logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
+                            <x-dropdown-link :href="route('admin.logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
